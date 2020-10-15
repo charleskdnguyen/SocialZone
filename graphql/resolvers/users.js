e8273e32b4e2985c1e347fe30dff5eebfea61106
Charles Nguyen <nguyencharles@live.ca>
2020-10-15 11:16:47 -0400

    Installed bcryptjs and jwt. Added register method

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,3 +1,9 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { UserInputError } = require('apollo-server-express')
+
+const { SECRET_KEY } = require('../../config')
+
 module.exports = {
   Query: {
     getUsers: async (_, __, context) => {
@@ -10,5 +16,42 @@ module.exports = {
         throw new Error(err);
       }
     }
+  },
+  Mutation: {
+    register: async (_, {
+      registerInput: {
+        username, email, password, confirmPassword
+      }
+    }, context) => {
+
+      //! validate user data
+
+      //! Make sure user doesn't already exist
+
+
+      // Hash password and create an auth token
+      password = await bcrypt.hash(password, 12);
+      const newUser = await context.prisma.user.create({
+        data: {
+          email,
+          username,
+          password,
+        }
+      });
+
+      const token = await jwt.sign({
+        id: newUser.id,
+        email: newUser.email,
+        username: newUser.username
+      }, SECRET_KEY, {
+        expiresIn: '1h'
+      });
+
+      return {
+        ...newUser,
+        id: newUser.id,
+        token,
+      };
+    }
   }
-}
+};
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -4,12 +4,24 @@ module.exports = gql`
   type Query {
     getUsers: [User]!
   }
+  
+  type Mutation {
+    register(registerInput: RegisterInput): User!
+  }
+  
+  input RegisterInput {
+    username: String!
+    password: String!
+    confirmPassword: String!
+    email: String!
+  }
 
   type User {
     id: ID!
     username: String!
-    password: String!
     email: String!
+    token: String!
+    createdAt: String!
   }
 
   type Post {
