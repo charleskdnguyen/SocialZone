1daf99efdd7d9615d656fb22826e01f5b7d48ce6
Charles Nguyen <nguyencharles@live.ca>
2020-10-19 18:07:43 -0400

    Added homepage cards and fetch posts function

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,62 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag';
+import { Grid } from 'semantic-ui-react'
+
+import PostCard from '../components/PostCard';
 
 function Home() {
+  const {
+    loading,
+    data,
+  } = useQuery(FETCH_POSTS_QUERY);
+
+  const fetchedPosts = data ? data.getPosts : null;
+
   return (
     <div>
-      <h1>Home Page</h1>
+      <Grid columns={3}>
+        <Grid.Row className="page-title">
+          <h1>Recent posts</h1>
+        </Grid.Row>
+        <Grid.Row>
+          { loading
+            ? (
+              <h1>Loading posts...</h1>
+            )
+            : (
+                fetchedPosts && fetchedPosts.map(post => (
+                <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+                  <PostCard post={post}/>
+                </Grid.Column>
+              ))
+            )
+          }
+        </Grid.Row>
+      </Grid>
     </div>
   )
 }
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id body createdAt likeCount
+      user {
+        id username
+      }
+      postLikes {
+        likedBy {
+          username
+        }
+      }
+      commentCount
+      postComments {
+        id createdAt body
+      }
+    }
+
+  }
+`
 
 export default Home;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 function Register() {
   return (
     <div>
