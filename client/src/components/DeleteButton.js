e8635e48a23c8d0d3f3dfd17c9baf00c5a387448
Charles Nguyen <nguyencharles@live.ca>
2020-10-21 20:06:24 -0400

    Added functionality to add new comment

--- client/src/components/DeleteButton.js
+++ client/src/components/DeleteButton.js
@@ -5,29 +5,34 @@ import { useMutation } from "@apollo/client";
 
 import { FETCH_POSTS_QUERY } from "../util/graphql";
 
-function DeleteButton({ postId, callback }) {
+function DeleteButton({ postId, commentId, callback }) {
 
   const [confirmOpen, setConfirmOpen] = useState(false);
 
-  const [deletePost] = useMutation(DELETE_POST_MUTATION, {
+  const mutation = commentId ? DELETE_COMMENT_MUTATION : DELETE_POST_MUTATION;
+
+  const [deletePostOrMutation] = useMutation(mutation, {
     update(proxy) {
       setConfirmOpen(false);
 
-      const data = proxy.readQuery({
-        query: FETCH_POSTS_QUERY,
-      })
+      if (!commentId) {
+        const data = proxy.readQuery({
+          query: FETCH_POSTS_QUERY,
+        })
 
-      data.getPosts = data.getPosts.filter(post => post.id !== postId)
+        data.getPosts = data.getPosts.filter(post => post.id !== postId)
 
-      proxy.writeQuery({
-        query: FETCH_POSTS_QUERY,
-        data
-      });
+        proxy.writeQuery({
+          query: FETCH_POSTS_QUERY,
+          data
+        });
+      }
 
       if (callback) callback();
     },
     variables: {
       postId,
+      commentId,
     }
   });
 
@@ -44,7 +49,7 @@ function DeleteButton({ postId, callback }) {
       <Confirm
         open={confirmOpen}
         onCancel={() => setConfirmOpen(false)}
-        onConfirm={deletePost}
+        onConfirm={deletePostOrMutation}
       />
     </>
   );
@@ -54,6 +59,19 @@ const DELETE_POST_MUTATION = gql`
   mutation deletePost($postId: ID!) {
     deletePost(id: $postId) {
       id
+    }
+  }
+`;
+
+const DELETE_COMMENT_MUTATION = gql`
+  mutation deletePost($postId: ID!, $commentId: ID!) {
+    deleteComment(postId: $postId, commentId: $commentId ) {
+      id
+      body
+      user {
+        id
+        username
+      }
     }
   }
 `;
--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -1,22 +1,38 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext, useRef } from 'react';
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
-import { Card, Image, Grid, Button, Icon, Label } from 'semantic-ui-react';
+import { Card, Image, Grid, Button, Icon, Label, Form } from 'semantic-ui-react';
 import moment from 'moment';
 import LikeButton from "../components/LikeButton";
 
 import { AuthContext } from "../context/auth";
 import DeleteButton from "../components/DeleteButton";
+import { useMutation } from "@apollo/client";
 
 function SinglePost(props) {
 
+  const [comment, setComment] = useState('');
+
   const { user } = useContext(AuthContext);
+
+  const commentInputRef = useRef(null);
 
   const postId = props.match.params.postId;
 
   const { data } = useQuery(FETCH_POST_QUERY, {
     variables: {
       postId
+    }
+  });
+
+  const [submitComment] = useMutation(SUBMIT_COMMENT_MUTATION, {
+    update() {
+      setComment('');
+      commentInputRef.current.blur();
+    },
+    variables: {
+      postId,
+      body: comment,
     }
   });
 
@@ -65,11 +81,50 @@ function SinglePost(props) {
                     {commentCount}
                   </Label>
                 </Button>
-                {/*{user && user.username === postComments.commentedBy.username && (*/}
+                {/*{user && user.username === fetchedPosts.post.user.username && (*/}
                 {/*  <DeleteButton postId={id} callback={deletePostCallback}/>*/}
                 {/*)}*/}
               </Card.Content>
             </Card>
+            {user && (
+              <Card fluid>
+                <Card.Content>
+                  <p>Post a comment</p>
+                  <Form>
+                    <div className="ui action input fluid">
+                      <input
+                        type="text"
+                        placeholder="Comment..."
+                        name="comment"
+                        value={comment}
+                        onChange={event => setComment(event.target.value)}
+                        ref={commentInputRef}
+                      />
+                      <button
+                        type="submit"
+                        className="ui button teal"
+                        disabled={comment.trim() === ''}
+                        onClick={submitComment}
+                      >
+                        Submit
+                      </button>
+                    </div>
+                  </Form>
+                </Card.Content>
+              </Card>
+            )}
+            {postComments.map(comment => (
+              <Card fluid key={comment.id}>
+                <Card.Content>
+                  {user && user.username === comment.commentedBy.username && (
+                    <DeleteButton postId={id} commentId={comment.id}/>
+                  )}
+                  <Card.Header>{comment.commentedBy.username}</Card.Header>
+                  <Card.Meta>{moment(comment.createdAt).fromNow()}</Card.Meta>
+                  <Card.Description>{comment.body}</Card.Description>
+                </Card.Content>
+              </Card>
+            ))}
           </Grid.Column>
         </Grid.Row>
       </Grid>
@@ -77,6 +132,23 @@ function SinglePost(props) {
   }
   return postMarkup;
 }
+
+const SUBMIT_COMMENT_MUTATION = gql`
+  mutation($postId: ID!, $body: String!) {
+    createComment(postId: $postId, body: $body) {
+      id
+      createdAt
+      body
+      commentedBy {
+        id
+        username
+      }
+      commentedPost {
+        commentCount
+      }
+    }
+  }
+`;
 
 const FETCH_POST_QUERY = gql`
   query($postId: ID!) {
